406d29e46d1cf09b61f5b5b435c16315db889189
liangchow <liangchern@gmail.com>
2025-10-29 22:20:28 -0700

    Add static root

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weather_app.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.static_ROOT)
